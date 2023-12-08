c08f40fc35a0ae394a2a6f6066e498a54bb6acf9
PUSHRSP <pushrsp@gmail.com>
2023-12-08 16:04:36 +0900

    getter, setter 추가

--- src/main/java/me/project/Node.java
+++ src/main/java/me/project/Node.java
@@ -5,15 +5,31 @@ public class Node<T extends Comparable<T>> {
     private Node<T> left;
     private Node<T> right;
 
+    public Node(T data) {
+        this.data = data;
+    }
+
     public T getData() {
         return this.data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
     }
 
     public Node<T> getLeft() {
         return this.left;
     }
 
+    public void setLeft(Node<T> left) {
+        this.left = left;
+    }
+
     public Node<T> getRight() {
         return this.right;
+    }
+
+    public void setRight(Node<T> right) {
+        this.right = right;
     }
 }
